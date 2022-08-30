9cb6992a2390133f44513cfe3336886c6262f756
maxHGR <maxhgr2@gmail.com>
2022-08-30 22:45:10 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import TodoList from './components/TodoList';
 
 const App = () => {
   const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText}/>
       <TodoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,34 @@
 import React from 'react';
 
+const Form = ({ setInputText }) => {
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    console.log('hey');
+  };
 
-const Form = () => {
-  const inputTextHandler = () => {
-    console.log(e);
-  }; 
-
-  return(
+  return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input"></input>
-        <button>
-          <i className="fas fa-plus-square"></i>
-        </button>
-        <div className="select">
-          <select name="todos" className="filter-todo">
-            <option value=""> All</option>
-            <option value="">Completed</option>
-            <option value="">Uncompleted</option>
-          </select>
-        </div>
+      <input
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      ></input>
+      <button onClick={submitTodoHandler} className="todo-button">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value=""> All</option>
+          <option value="">Completed</option>
+          <option value="">Uncompleted</option>
+        </select>
+      </div>
     </form>
-  )
-}
+  );
+};
 
 export default Form;
