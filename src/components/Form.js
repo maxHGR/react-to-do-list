8404c1c78a4b72ee379ba77bb62fbbb62bb88a2b
maxHGR <maxhgr2@gmail.com>
2022-08-30 22:01:37 +0200

    2 files modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,15 +4,16 @@ import React from 'react';
 const Form = () => {
   return(
     <form>
-      <input type="text" className="todo-input"/>
+      <input type="text" className="todo-input"></input>
         <button>
           <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">
-          <select name="todos" className="filter-todo"/>
+          <select name="todos" className="filter-todo">
             <option value=""> All</option>
             <option value="">Completed</option>
             <option value="">Uncompleted</option>
+          </select>
         </div>
     </form>
   )
