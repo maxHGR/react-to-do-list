c4d103bfcea322ddb3830c7ff503253f735497e3
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-03 00:43:13 +0200

    3 files modified

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
-const Todo = ({ text }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="todo-item-container">
       <li className="todo-item">{text}</li>
-      <button
-        className="complete-btn">
+      <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
