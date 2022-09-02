3b116807b7655c93908d079bb4bafc8bc212091d
maxHGR <93156633+maxHGR@users.noreply.github.com>
2022-09-03 01:40:28 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  //State Stuff
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
+  //Functions
+
+const filterHandler =()=> {
+  switch(status) {
+    case 'completed':
+      setFilteredTodos(todos.filter(todo => todo.completed === true))
+      ;break;
+    case 'uncompleted':
+      setFilteredTodos(todos.filter(todo => todo.completed === false));
+      break;
+    default: setFilteredTodos(todos);
+      break;
+  }
+}
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form 
-      inputText={inputText} 
-      todos={todos} 
-      setTodos={setTodos} 
-      setInputText={setInputText}/>
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 };
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Form = ({ inputText, setInputText, todos, setTodos }) => {
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
   const inputTextHandler = (e) => {
-    
+
     setInputText(e.target.value);
   };
   const submitTodoHandler = (e) => {
@@ -18,6 +18,10 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
     setInputText('');
   };
 
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  }
+
   return (
     <form>
       <input
@@ -31,7 +35,7 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all"> All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
